3a70eb327508f89afffd5782993e7b6281a229d2
TzuYi Chao <tzuyichao0313@gmail.com>
2023-03-10 14:44:35 +0800

    App Configuration with Label lab

---
+++ app-configuration/hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+public static class Demo
+{
+    static void Main(string[] args) 
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddAzureAppConfiguration(options => {
+            options
+                .Connect(Environment.GetEnvironmentVariable("ConnectionString"))
+                .Select(KeyFilter.Any, "lab");
+        });
+
+        var config = builder.Build();
+        Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello World!");
+    }
+}
---
+++ app-configuration/hello/README.md
@@ -0,0 +1,3 @@
+# App Configuration with Label
+
+這個範例跟官網例子一樣透過環境變數拿Connection String，只是會要Label = lab的key。
